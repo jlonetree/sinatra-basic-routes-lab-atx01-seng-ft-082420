c656625ecc4963e15538b045de0e6fc4bf8056fd
jlonetree <jlonetree@gmail.com>
2020-09-12 17:51:52 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do 
+        "Hello, World!"
+    end
+
+    get '/name' do 
+        "My name is John"
+    end
+
+    get '/hometown' do
+        "My hometown is Reedsburg, WI"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Pockets by Easy Life"
+    end
 end
